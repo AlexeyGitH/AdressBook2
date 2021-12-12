ccf0d6ea5b96565698ca7f6b95496a00d07e6611
AlexeyGitH <kharlamov.a.y@gmail.com>
2021-12-13 00:21:35 +0300

    d

--- client_flutter/lib/screens/contacts.dart
+++ client_flutter/lib/screens/contacts.dart
@@ -54,14 +54,43 @@ class _DataViewList extends State<DataViewList> {
         blockrightarrow: false,
         viewResume: 0);
 
+    var screenSize = MediaQuery.of(context).size;
+    var cardNumber = (screenSize.width~/Width_card_const == 0) ? 1: screenSize.width~/Width_card_const;
+
+    print('screenSize: '+ screenSize.width.toString());
+    print('cardNumber (500): '+ cardNumber.toString());
+
+    if (screenSize.width > 1000) {
+      return DataViewListTwo(filters: filters, dataBaseData: dataBaseData, cardNumber: cardNumber);
+    }else
+    //{return DataViewListOne(filters: filters, dataBaseData: dataBaseData);}
+    {return DataViewListTwo(filters: filters, dataBaseData: dataBaseData, cardNumber: cardNumber);}
+
+  }
+}
+
+class DataViewListOne extends StatefulWidget {
+  FiltersModel filters;
+  DataBaseData dataBaseData;
+
+  DataViewListOne({required this.filters, required this.dataBaseData});
+
+  @override
+  _DataViewListOne createState() => _DataViewListOne();
+}
+
+class _DataViewListOne extends State<DataViewListOne> {
+  @override
+  Widget build(BuildContext context) {
+
     return FutureBuilder(
-        future: getContactList(filters),
-        initialData: dataBaseData,
+        future: getContactList(widget.filters),
+        initialData: widget.dataBaseData,
         builder: (BuildContext context, AsyncSnapshot<DataBaseData> snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
               return SingleChildScrollView(child:
-                Column(children: [
+              Column(children: [
                 SizedBox(
                   height: MediaQuery.of(context).size.height / 1.3 - 20,
                   child: Center(
@@ -77,39 +106,39 @@ class _DataViewList extends State<DataViewList> {
               );
             default:
               if (snapshot.hasError)
-                return RefreshWidget(changeValueView: filters.setviewResume);
+                return RefreshWidget(changeValueView: widget.filters.setviewResume);
               else if (snapshot.data == null) {
-                return RefreshWidget(changeValueView: filters.setviewResume);
+                return RefreshWidget(changeValueView: widget.filters.setviewResume);
               } else {
                 DataBaseData? vDBD = snapshot.data;
                 if (vDBD == null) {
-                  return RefreshWidget(changeValueView: filters.setviewResume);
+                  return RefreshWidget(changeValueView: widget.filters.setviewResume);
                 } else {
                   if (vDBD.viewResume == 1) {
                     return
                       Column(
-                      //mainAxisSize: MainAxisSize.min,
-                        children: [
+                        //mainAxisSize: MainAxisSize.min,
+                          children: [
                             Expanded(child :
                             ListPageList(
-                                serverdata: vDBD.database.contacts,
+                              serverdata: vDBD.database.contacts,
                             )),
-                          Divider(height: 1, color: Colors.blueGrey),
-                          Align(
-                            alignment: FractionalOffset.bottomCenter,
-                            child: Row(
-                              children: [
-                                ArrowBottomWidget(0, false, filters.datalistcount, vDBD.database.countlist, filters.contactsChangeRange),
-                                Spacer(),
-                                ArrowBottomWidget(1, vDBD.blockrightarrow, filters.datalistcount, vDBD.database.countlist, filters.contactsChangeRange),
-                              ],
-                            ),
-                          ),                    ]);
+                            Divider(height: 1, color: Colors.blueGrey),
+                            Align(
+                              alignment: FractionalOffset.bottomCenter,
+                              child: Row(
+                                children: [
+                                  ArrowBottomWidget(0, false, widget.filters.datalistcount, vDBD.database.countlist, widget.filters.contactsChangeRange),
+                                  Spacer(),
+                                  ArrowBottomWidget(1, vDBD.blockrightarrow, widget.filters.datalistcount, vDBD.database.countlist, widget.filters.contactsChangeRange),
+                                ],
+                              ),
+                            ),                    ]);
 
 
                   } else {
                     return RefreshWidget(
-                        changeValueView: filters.setviewResume);
+                        changeValueView: widget.filters.setviewResume);
                   }
                 }
                 return Text('Result: ${snapshot.data}');
@@ -119,6 +148,94 @@ class _DataViewList extends State<DataViewList> {
         });
   }
 }
+
+class DataViewListTwo extends StatefulWidget {
+  FiltersModel filters;
+  DataBaseData dataBaseData;
+  int cardNumber;
+
+  DataViewListTwo({required this.filters, required this.dataBaseData, required this.cardNumber});
+
+  @override
+  _DataViewListTwo createState() => _DataViewListTwo();
+}
+
+class _DataViewListTwo extends State<DataViewListTwo> {
+  @override
+  Widget build(BuildContext context) {
+
+    return FutureBuilder(
+        future: getContactList(widget.filters),
+        initialData: widget.dataBaseData,
+        builder: (BuildContext context, AsyncSnapshot<DataBaseData> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return SingleChildScrollView(child:
+              Column(children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 1.3 - 20,
+                  child: Center(
+                    child: SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: CircularProgressIndicator()),
+                  ),
+                ),
+                const Divider(height: 4, color: Colors.black),
+                Text('Loading..', style: Theme.of(context).textTheme.headline5),
+              ])
+              );
+            default:
+              if (snapshot.hasError)
+                return RefreshWidget(changeValueView: widget.filters.setviewResume);
+              else if (snapshot.data == null) {
+                return RefreshWidget(changeValueView: widget.filters.setviewResume);
+              } else {
+                DataBaseData? vDBD = snapshot.data;
+                if (vDBD == null) {
+                  return RefreshWidget(changeValueView: widget.filters.setviewResume);
+                } else {
+                  if (vDBD.viewResume == 1) {
+                    return
+                      Column(
+                        //mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(child :
+                            GridPageList(
+                            //  GridPageListCopy(
+                                serverdata: vDBD.database.contacts, cardNumber:widget.cardNumber
+                            ),
+                            //ListPageList(serverdata: vDBD.database.contacts),
+
+                             ),
+                            Divider(height: 1, color: Colors.blueGrey),
+                            Align(
+                              alignment: FractionalOffset.bottomCenter,
+                              child: Row(
+                                children: [
+                                  ArrowBottomWidget(0, false, widget.filters.datalistcount, vDBD.database.countlist, widget.filters.contactsChangeRange),
+                                  Spacer(),
+                                  ArrowBottomWidget(1, vDBD.blockrightarrow, widget.filters.datalistcount, vDBD.database.countlist, widget.filters.contactsChangeRange),
+                                ],
+                              ),
+                            ),                    ]);
+
+
+                  } else {
+                    return RefreshWidget(
+                        changeValueView: widget.filters.setviewResume);
+                  }
+                }
+                return Text('Result: ${snapshot.data}');
+              }
+          }
+
+        });
+  }
+}
+
+
+
 
 class RefreshWidget extends StatefulWidget {
   final Function(int) changeValueView;
@@ -425,5 +542,278 @@ class ArrowBottomWidget extends StatelessWidget {
             }
           }),
     );
+  }
+}
+
+
+
+
+
+
+class GridPageListCopy extends StatefulWidget {
+  List<ContactItem> serverdata;
+  int cardNumber;
+  GridPageListCopy({required this.serverdata, required this.cardNumber});
+
+  @override
+  _GridPageListCopy createState() => _GridPageListCopy();
+}
+
+class _GridPageListCopy extends State<GridPageListCopy> {
+  @override
+  Widget build(BuildContext context) {
+
+    return GridView.count(crossAxisCount: widget.cardNumber,
+
+        //childAspectRatio: MediaQuery.of(context).size.width /
+        //    (MediaQuery.of(context).size.height / 1),
+        childAspectRatio:1/2.5,
+
+        controller: new ScrollController(keepScrollOffset: false),
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+
+
+        children:
+
+
+        List.generate(widget.serverdata.length, (index) {
+          var postPone = widget.serverdata[index];
+
+          List<ItemCard> cardContact = [];
+          cardContact.add(ItemCard(name: "Организация", icon: '', value: postPone.corporation.toString()));
+          cardContact.add(ItemCard(name: "Должность", icon: '', value: postPone.position.toString()));
+          cardContact.add(ItemCard(name: "Подразделение", icon: '', value: postPone.department.toString()));
+          cardContact.add(ItemCard(name: "Дата рождения", icon: '', value: postPone.birthdate.toString()));
+          cardContact.add(ItemCard(name: "Рабочий тел.", icon: 'p', value: postPone.workphone.toString()));
+          cardContact.add(ItemCard(name: "Мобильный тел.", icon: 'p', value: postPone.mobilephone.toString()));
+          cardContact.add(ItemCard(name: "Почта", icon: 'e', value: postPone.mail.toString()));
+
+
+
+          return new
+
+
+
+
+          Container(
+              decoration: myBoxDecoration(),
+              margin: const EdgeInsets.all(6.0),
+              padding: const EdgeInsets.all(3.0),
+              child: Column(children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    //Expanded(
+                    //child:
+                    new ConstrainedBox(
+                        constraints: new BoxConstraints(
+                          maxHeight: 310.0,
+                        ),
+                        child:
+
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                            child:
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child:
+                                Image.network(
+                                  'http://'+ipLocalhost + postPone.photo.toString(),
+                                  //'',
+                                  //fit: BoxFit.fitHeight,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    //print(error);
+                                    return Image.asset(
+                                      'assets/NoPhoto.png',
+                                      //fit: BoxFit.fitHeight,
+                                    );
+                                  },
+                                )
+
+                            )
+
+
+
+                        )),
+                    // ),
+                    Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            Row(children: [
+                              Expanded(
+                                  child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: RichText(
+                                        text: TextSpan(
+                                          text: 'Статус: ',
+                                          style: new TextStyle(
+                                              fontSize: 12.0,
+                                              color: Colors.black),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                                text: postPone.status
+                                                    .toString(),
+                                                style: TextStyle(
+                                                    fontSize: 12.0,
+                                                    color:
+                                                    Colors.green[900])),
+                                          ],
+                                        ),
+                                      ))),
+                            ]),
+                            Row(children: [
+                              Expanded(
+                                  child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        postPone.firstname.toString() +
+                                            " " +
+                                            postPone.middlename.toString() +
+                                            " " +
+                                            postPone.lastname.toString(),
+                                        style: new TextStyle(
+                                          //backgroundColor: Colors.blue,
+                                          fontFamily: 'Quicksand',
+                                          fontSize: 20.0,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ))),
+                            ])
+                          ],
+                        )),
+                  ],
+                ),
+                ContactValues(cardContact),
+
+              ]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+          ;
+        }
+
+
+
+        )
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+class GridPageList extends StatefulWidget {
+  List<ContactItem> serverdata;
+  int cardNumber;
+  GridPageList({required this.serverdata, required this.cardNumber});
+
+  @override
+  _GridPageList createState() => _GridPageList();
+}
+
+class _GridPageList extends State<GridPageList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: widget.serverdata.length,
+        itemBuilder: (context, index) {
+          var postPone = widget.serverdata[index];
+
+          List<ItemCard> cardContact = [];
+          cardContact.add(ItemCard(
+              name: "Организация",
+              icon: '',
+              value: postPone.corporation.toString()));
+          cardContact.add(ItemCard(
+              name: "Должность",
+              icon: '',
+              value: postPone.position.toString()));
+          cardContact.add(ItemCard(
+              name: "Подразделение",
+              icon: '',
+              value: postPone.department.toString()));
+          cardContact.add(ItemCard(
+              name: "Дата рождения",
+              icon: '',
+              value: postPone.birthdate.toString()));
+          cardContact.add(ItemCard(
+              name: "Рабочий тел.",
+              icon: 'p',
+              value: postPone.workphone.toString()));
+          cardContact.add(ItemCard(
+              name: "Мобильный тел.",
+              icon: 'p',
+              value: postPone.mobilephone.toString()));
+          cardContact.add(ItemCard(
+              name: "Почта", icon: 'e', value: postPone.mail.toString()));
+
+          //print(ipLocalhost + postPone.photo.toString());
+          if (index%widget.cardNumber==0) {
+
+          }
+
+          List<Widget> wChildren = [];
+          for (var i = 0; i < 2; i++)
+            {
+              List<Widget> d = [];
+              for (var w = 0; w < 2; w++)
+
+                {
+     
+                  d.add(Text((w+i).toString()));
+                }
+              wChildren.add(Row(children: d));
+            }
+
+return Column(children: wChildren);
+
+
+          //  return Text('ff');
+
+/*
+          new ListView.builder
+            (
+              itemCount: litems.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return new Text(litems[index]);
+              }
+
+
+*/
+        /*  return Column(
+            children: [Row(children:[Text('1'), Text('2')]), Row(children:[Text('3'), Text('4')])]
+          );*/
+
+
+        });
+
+
+
   }
 }
