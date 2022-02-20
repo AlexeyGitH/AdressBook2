38b5b825af03049dbe26c88220864379ca735aef
AlexeyGitH <kharlamov.a.y@gmail.com>
2022-02-21 00:02:53 +0300

    f

--- Admin/lib/screens/MainAdminPage.dart
+++ Admin/lib/screens/MainAdminPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:admin/models/mainStatesModel.dart';
 
@@ -10,97 +11,220 @@ class MainAdminPage extends StatefulWidget {
   _MainAdminPage createState() => _MainAdminPage();
 }
 
-
 class _MainAdminPage extends State<MainAdminPage> {
+  final ScrollController _firstController = ScrollController();
+
 
   Widget build(BuildContext context) {
-    var mainConstModel = context.watch<MainConstModel>();
-
-    final _storage = const FlutterSecureStorage();
-    Future<void> _deleteValStorage() async {
-      await _storage.delete(key: 'fff');
-    }
-
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text("Admin 2"),
+          title: Text("Main page"),
         ),
-        body: SingleChildScrollView(
-            child: Row (children: [
-              Expanded(child: Column()),
-              Expanded(flex:5, child: Column(children: [
-                Center(
-                  child:
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 30.0),
-                    child: Center(
-                      child: Container(
-                          width: 200,
-                          height: 150,
-                          child: Image.asset('logo.png')),
+        body:
+Column(children: [
+
+        Scrollbar(
+        isAlwaysShown: screenSize.width <= 600 ? true:false,
+        controller: _firstController,
+        child:
+
+        SingleChildScrollView(
+        scrollDirection: Axis. horizontal,
+        controller: _firstController,
+        child:
+
+
+    Table(
+
+      columnWidths: const <int, TableColumnWidth>{
+        0: FixedColumnWidth(600),
+        1: FixedColumnWidth(300),
+        2: FixedColumnWidth(300),
+        3: FixedColumnWidth(300),
+        4: FixedColumnWidth(700),
+        5: FixedColumnWidth(700),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: <TableRow>[
+        TableRow(
+          children: <Widget>[
+            TableCell(
+              decoration: BoxDecoration(color: Colors.grey[200]),
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('ФИО'))),
+            ),
+            TableCell(
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('Организация'))),
+            ),
+            TableCell(
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('Подразделение'))),
+            ),
+            TableCell(
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('Должность'))),
+            ),
+            TableCell(
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('Должность'))),
+            ),
+            TableCell(
+              child: Container(padding: const EdgeInsets.all(5), child: Center(child:Text('Должность'))),
+            ),
+
+          ],
+        ),
+      ],
+    ),
+
+        ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+        Scrollbar(
+            isAlwaysShown: screenSize.width <= 600 ? true:false,
+            controller: _firstController,
+            child:
+
+        SingleChildScrollView(
+          scrollDirection: Axis. horizontal,
+            controller: _firstController,
+            child:
+
+
+
+
+            Column(children: [
+
+          Row(
+
+            children: [
+                Container(
+                    width: 200,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                          left: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                          right: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                          bottom: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        ),
+                      ),
+                      //margin: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.all(5),
+
+                    child: Row(
+
+                      children: [Expanded(child: Center(child:Text('ФИО'))), Text('ff')],)
+                  ),
+              SizedBox(
+                width: 200,
+                child:
+                Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        right: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        bottom: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 15.0, right: 15.0, top: 15, bottom: 0),
-                  //padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Login2',
-                        hintText: 'Enter login22'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 15.0, right: 15.0, top: 15, bottom: 0),
-                  //padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: TextField(
+                    //margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(5),
 
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                        hintText: 'Enter password'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 0, right: 0, top: 40, bottom: 0),
-                  //padding: EdgeInsets.symmetric(horizontal: 15),
-                  child:
-
-                  TextButton(
-                    onPressed: () {
-//                      mainConstModel.setAuthenticated(false);
-                      _deleteValStorage();
-                      debugPrint('Delete storage');
-                      //String base64Auth = stringToBase64.encode("${login}:${password}");
-                      //Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => MyHomePage(title: 'fff'),), (route) => false);
-
-                      //Navigator.push(
-                      //    context, MaterialPageRoute(builder: (_) => MyHomePage(title: 'fff',)));
-                    },
-                    child: Text(
-                      'Login',
-                      style: TextStyle(color: Colors.black38, fontSize: 25),
-                    ),
-                  ),
+                    child: Row(children: [Expanded(child: Center(child:Text('ФИО dfd dfdfgd dfgdf fdgdfgd fdsfs sdfsd sdfsd sdfsfsd'))), Text('ff')],)),
               ),
 
 
+                  Column(
+                    children: [
+                      Text('ФИО'),
+                    ],
+                  ),
 
-              ])),
-              Expanded(child: Column()),
 
 
-            ])
+
+              SizedBox(
+                  width: 400,
+                  child: Column(
+                    children: [
+                      Text('Организация'),
+                    ],
+                  )),
+              SizedBox(
+                  width: 400,
+                  child: Column(
+                    children: [
+                      Text('Подразделение'),
+                    ],
+                  )),
+              SizedBox(
+                  width: 500,
+                  child: Column(
+                    children: [
+                      Text('Должность'),
+                    ],
+                  )),
+
+              SizedBox(
+                width: 600,
+                child:
+                Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        left: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        right: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        bottom: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                      ),
+                    ),
+                    margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(5),
+
+                    child: Text('ФИО')),
+              ),
+              SizedBox(
+                width: 600,
+                child:
+                Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        left: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        right: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                        bottom: BorderSide(width: 1.0, color: Color(0xFF000000)),
+                      ),
+                    ),
+                    margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(5),
+
+                    child: Text('ФИО')),
+              ),
+            ],
+          )
+        ])
+
+    )
         )
+*/
 
+
+],)
     );
   }
-
 }
-
