bace3c9634f50734dc9a4df054a889fea1c2c5f2
AlexeyGitH <kharlamov.a.y@gmail.com>
2022-03-24 23:53:09 +0300

    f

---
+++ Admin/lib/screens/CardContact.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:admin/models/mainStatesModel.dart';
+import 'dart:async';
+import 'package:admin/models/database.dart';
+import 'package:admin/ConstSystemAD.dart';
+import 'package:admin/main.dart';
+
+class CardContact extends StatefulWidget {
+  @override
+  _CardContact createState() => _CardContact();
+}
+
+class _CardContact extends State<CardContact> {
+
+  Widget build(BuildContext context) {
+    return Text('fff');
+  }
+}
